8018fd52144c0e429137c7cd4def365433c9aeae
TomSparto <tasparto@hotmail.com>
2021-10-20 17:26:41 -0400

    refactored default case to display retroactive amount of users based on length of array

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -26,6 +26,8 @@ function likes(names) {
     case 3:
       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
     default:
-      return `${names[0]}, ${names[1]} and 2 others like this`;
+      return `${names[0]}, ${names[1]} and ${
+        names.length - 2
+      } others like this`;
   }
 }
