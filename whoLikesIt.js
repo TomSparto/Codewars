ffeb95c4279a80c9372d1b84ba0ae1a1a3c57792
TomSparto <tasparto@hotmail.com>
2021-10-20 17:20:40 -0400

    whoLikesIt passes all tests using if statements to return all cases

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -1,7 +1,10 @@
 /*
-You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+You probably know the "like" system from Facebook and other pages. 
+People can "like" blog posts, pictures or other items. 
+We want to create the text that should be displayed next to such an item.
 
-Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+Implement the function which takes an array containing the names of people that like an item. 
+It must return the display text as shown in the examples:
 
 []                                -->  "no one likes this"
 ["Peter"]                         -->  "Peter likes this"
@@ -11,3 +14,21 @@ Implement the function which takes an array containing the names of people that 
 
 Note: For 4 or more names, the number in "and 2 others" simply increases.
 */
+
+function likes(names) {
+  if (names.length === 0) {
+    return "no one likes this";
+  }
+  if (names.length === 1) {
+    return `${names[0]} likes this`;
+  }
+  if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  }
+  if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  }
+  if (names.length > 3) {
+    return `${names[0]}, ${names[1]} and 2 others like this`;
+  }
+}
